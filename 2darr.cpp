11cea158537f1f90e55084f40874b6ba8c74d389
tuongwall <tuongwall151@gmail.com>
2025-08-02 18:27:41 +0700

    to update

--- 2darr.cpp
+++ 2darr.cpp
@@ -4,6 +4,7 @@
 #include <cstring>
 #include <map>
 #include <cmath>
+#include <iomanip>
 using namespace std;
 void arr_random(int a[100][100], int n, int m){
     random_device rd;
@@ -17,6 +18,9 @@ void arr_random(int a[100][100], int n, int m){
 }
 //function check prime number
 bool check(int n){
+    if (n < 2){
+        return false;
+    }
     for (int i = 2; i < sqrt(n); i++){
         if(n % i == 0){
             return false;
@@ -32,7 +36,7 @@ int main(){
     // input number
     cout << "Enter the number n = ";
     cin >> n;
-    cout << "----------------------" << endl;
+    cout << "----------------------\n";
     cout << "Enter the number m = ";
     cin >> m;
     arr_random(a, n, m); // random 2d array
@@ -63,29 +67,29 @@ int main(){
     }
 
     // output array
-    cout <<"-----------------------------" << endl;
-    cout << "The Output Of A Random 2D Array has elements [i][j]: " << endl;
-    cout << "---------------------------" << endl;
+    cout <<"-----------------------------\n";
+    cout << "The Output Of A Random 2D Array has elements [i][j]: \n";
+    cout << "---------------------------\n";
     for (int i = 0; i < n; i++){
         for (int j = 0; j < m; j++){
             cout << "2D Array [i][j]: " << "[ " << i << " ]" << "[ " << j << " ]" << " = " << a[i][j] << endl;
         }
     }
 
-    cout << "---------------------------" << endl;
-    cout << "Random 2D Array Output " << endl;
-    cout << "---------------------------" << endl;
+    cout << "---------------------------\n";
+    cout << "Random 2D Array Output \n";
+    cout << "---------------------------\n";
     for (int i = 0; i < n; i++){
         for (int j = 0; j < m; j++){
-            cout << a[i][j] << "\t";
+            cout << setw(4) << a[i][j];
         }
         cout << endl;
     }
     //total array 
     cout << endl;
-    cout << "---------------------------" << endl;
-    cout << "Total Of The Whole 2D Array: " << endl;
-    cout << "---------------------------" << endl;
+    cout << "---------------------------\n";
+    cout << "Total Of The Whole 2D Array: \n";
+    cout << "---------------------------\n";
     int total = 0;
     for (int i = 0; i < n; i++){
         for (int j = 0; j < m; j++){
@@ -95,9 +99,9 @@ int main(){
     cout << "Total 2D Array: " << total << endl;
     cout << endl;
     // total row array
-    cout << "----------------------------" << endl;
-    cout << "Total Row Of 2D Array: " << endl;
-    cout << "---------------------------" << endl;
+    cout << "----------------------------\n";
+    cout << "Total Row Of 2D Array: \n";
+    cout << "---------------------------\n";
     for (int i = 0; i < n; i++){
         int row_sum = 0;
         for (int j = 0; j < m; j++){
@@ -107,9 +111,9 @@ int main(){
     }
     cout << endl;
     // total column array
-    cout << "---------------------------" << endl;
-    cout << "Total Column Of 2D Array: " << endl;
-    cout << "---------------------------" << endl;
+    cout << "---------------------------\n";
+    cout << "Total Column Of 2D Array: \n";
+    cout << "---------------------------\n";
     for (int j = 0; j < m; j++){
         int col_sum = 0;
         for (int i = 0; i < n; i++){
@@ -117,11 +121,37 @@ int main(){
         }
         cout << "Column 2D Array " << j + 1 << ": " << col_sum << endl;
     }
+    // average row array
+    cout << endl;
+    cout << "---------------------------\n";
+    cout << "Average row of 2d array\n";
+    cout << "---------------------------\n";
+    for (int i = 0; i < n; i++){
+        int row_ave = 0;
+        for (int j = 0; j < m; j++){
+            row_ave += a[i][j];
+        }
+        double row_average = (double)row_ave / m;
+        cout << "Average row array " << row_average << endl;
+    }
+    //average column array
+    cout << endl;
+    cout << "---------------------------\n";
+    cout << "Average column of 2d array\n";
+    cout << "---------------------------\n";
+    for (int j = 0; j < m; j++){
+        int col_ave = 0;
+        for (int i = 0; i < n; i++){
+            col_ave += a[i][j];
+        }
+        double col_average = (double)col_ave;
+        cout << "Average column array " << col_average << endl;
+    }
     // max and min array
     cout << endl;
-    cout << "---------------------------" << endl;
-    cout << "The Maxval And Minval In The 2D Array" << endl;
-    cout << "---------------------------" << endl;
+    cout << "---------------------------\n";
+    cout << "The Maxval And Minval In The 2D Array\n";
+    cout << "---------------------------\n";
     for (int i = 0; i < n; i++){
         for (int j = 0; j < m; j++){
             if (a[i][j] > maxval){
@@ -132,20 +162,20 @@ int main(){
             }
         }
     }
-    cout << "Max lvalue number " << maxval << endl;
-    cout << "---------------------------" << endl;
-    cout << "Min lvalue number " << minval << endl;
-    cout << "---------------------------" << endl;
+    cout << "Max lvalue number " << maxval << '\n';
+    cout << "---------------------------\n";
+    cout << "Min lvalue number " << minval << '\n';
+    cout << "---------------------------\n";
     //total max and min
     cout << endl;
-    cout << "----------------------------" << endl;
-    cout << "Total maxval and minval " << maxval + minval << endl;
-    cout << "----------------------------" << endl;
+    cout << "----------------------------\n";
+    cout << "Total maxval and minval " << maxval + minval << '\n';
+    cout << "----------------------------\n";
     cout << endl;
     // even and odd
-    cout << "-----------------------------" << endl;
-    cout << "Count The Number Of Even And Odd" << endl;
-    cout << "-----------------------------" << endl;
+    cout << "-----------------------------\n";
+    cout << "Count The Number Of Even And Odd\n";
+    cout << "-----------------------------\n";
     int even_number = 0;
     int odd_number = 0;
     for (int i = 0; i < n; i++){
@@ -158,23 +188,34 @@ int main(){
             }
         }
     }
-    cout << "Number Of Even Numbers " << even_number << "  Number" << endl;
-    cout << "------------------------" << endl;
-    cout << "Number Of Odd Numbers " << odd_number << " Number" << endl;
-    cout << "----------------------------" << endl;
+    cout << "Number Of Even Numbers " << even_number << "  Number\n";
+    cout << "------------------------\n";
+    cout << "Number Of Odd Numbers " << odd_number << " Number\n";
+    cout << "----------------------------\n";
     cout << endl;
-    cout << "----------------------------" << endl;
+    cout << "----------------------------\n";
     //check prime numbers 
-    cout << "Prime Number" << endl;
-     cout << "----------------------------" << endl;
+    cout << "Prime Number\n";
+    cout << "----------------------------\n";
     map <int, bool> mp;
+    int count = 0;
     for (int i = 0; i < n; i++){
         for (int j = 0; j < m; j++){
             if (check(a[i][j]) && mp[a[i][j]] == false){
                 cout << a[i][j] << " ";
+                count++;
                 mp[a[i][j]] = true;
             }
         }
         cout << endl;
     }
+    cout << "There is a total number of primes " << count << endl; 
+    cout << "----------------------------------\n";
+    // the diagonal of the 2d array
+    cout << "The diagonal of the 2d array\n";
+    for (int i = 0; i < n && i < m; i++){
+        cout << "[ " << i << " ]" << "[ " << i << " ]" << " = " << a[i][i] << endl; 
+    }
+    cout << "---------------------------------\n";
+    
 }
